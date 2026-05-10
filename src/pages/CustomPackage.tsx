c25593cb222b9859119e56ebddb54d828594e73b
Haj Sandeepa <hajsandeepa@gmail.com>
2026-05-10 18:42:54 +0530

    fix scroll up button issue

--- src/pages/CustomPackage.tsx
+++ src/pages/CustomPackage.tsx
@@ -9,7 +9,6 @@ import Navbar from "@/components/Navbar";
 import PageHero from "@/components/PageHero";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
-import ScrollToTop from "@/components/ScrollToTop";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -451,7 +450,6 @@ Looking forward to your response!
 
       <Footer />
       <WhatsAppButton />
-      <ScrollToTop />
     </div>
   );
 };
